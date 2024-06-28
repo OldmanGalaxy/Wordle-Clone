abb56a369e6de34655d280696e9b5a71f33cfe63
purv-kabaria <153810916+OldmanGalaxy@users.noreply.github.com>
2024-06-28 18:14:58 +0530

    add: head and guessbox

---
+++ src/Body.jsx
@@ -0,0 +1,50 @@
+export default function Body() {
+  return (
+    <>
+    <div className='guesses-container'>
+      <div className='row1'>
+        <div className='cell-11 cell'></div>
+        <div className='cell-12 cell'></div>
+        <div className='cell-13 cell'></div>
+        <div className='cell-14 cell'></div>
+        <div className='cell-15 cell'></div>
+      </div>
+      <div className='row2'>
+        <div className='cell-21 cell'></div>
+        <div className='cell-22 cell'></div>
+        <div className='cell-23 cell'></div>
+        <div className='cell-24 cell'></div>
+        <div className='cell-25 cell'></div>
+      </div>
+      <div className='row3'>
+        <div className='cell-31 cell'></div>
+        <div className='cell-32 cell'></div>
+        <div className='cell-33 cell'></div>
+        <div className='cell-34 cell'></div>
+        <div className='cell-35 cell'></div>
+      </div>
+      <div className='row4'>
+        <div className='cell-41 cell'></div>
+        <div className='cell-42 cell'></div>
+        <div className='cell-43 cell'></div>
+        <div className='cell-44 cell'></div>
+        <div className='cell-45 cell'></div>
+      </div>
+      <div className='row5'>
+        <div className='cell-51 cell'></div>
+        <div className='cell-52 cell'></div>
+        <div className='cell-53 cell'></div>
+        <div className='cell-54 cell'></div>
+        <div className='cell-55 cell'></div>
+      </div>
+      <div className='row6'>
+        <div className='cell-61 cell'></div>
+        <div className='cell-62 cell'></div>
+        <div className='cell-63 cell'></div>
+        <div className='cell-64 cell'></div>
+        <div className='cell-65 cell'></div>
+      </div>
+    </div>
+    </>
+  )
+};
---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+    return (
+        <>
+            <header>
+                <div className='header-container'>
+                    <div className='title'>
+                        Wordle
+                    </div>
+                </div>
+            </header>
+        </>
+    )
+};
